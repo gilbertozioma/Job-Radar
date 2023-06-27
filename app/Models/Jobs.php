080f20891079ce91fec69b917f40e87e91f9a57b
Your Name <gilbertozioma0@gmail.com>
2023-06-28 00:47:48 +0100

    Added comments to the Jobs model

--- app/Models/Jobs.php
+++ app/Models/Jobs.php
@@ -11,25 +11,30 @@ class Jobs extends Model
 
     protected $fillable = ['title', 'user_id', 'company', 'location', 'website', 'email', 'description', 'tags', 'logo'];
 
-    public function scopeFilter($query, array $filters){
-        // ?? Null Coalescing operator
-        if($filters['tag'] ?? false){
+    // The scopeFilter method allows you to filter the query based on certain criteria.
+    public function scopeFilter($query, array $filters)
+    {
+        // Null Coalescing operator (??) is used to check if the 'tag' key is present in the $filters array.
+        if ($filters['tag'] ?? false) {
+            // The query will search for records where the 'tags' column contains the specified tag.
             $query->where('tags', 'like', '%' . request('tag') . '%');
         }
 
-        if($filters['search'] ?? false){
+        // Null Coalescing operator (??) is used to check if the 'search' key is present in the $filters array.
+        if ($filters['search'] ?? false) {
+            // The query will search for records where the 'title', 'description', or 'tags' column contains the specified search term.
             $query->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('description', 'like', '%' . request('search') . '%')
-            ->orWhere('tags', 'like', '%' . request('search') . '%')
-            ;
+                ->orWhere('description', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
     }
 
-    public function user(){
+    // The Jobs model belongs to a User model. The 'user_id' column is used as the foreign key.
+    public function user()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 }
-
 
 // php artisan tinker
 // \App\Models\Jobs::first()
